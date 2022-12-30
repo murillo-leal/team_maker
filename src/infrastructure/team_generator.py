113320521e9d2fb8ad72dc0868de8a618beb2f68
Murillo <murilloadcl@msn.com>
2022-12-30 17:24:12 -0200

    generate_team added

--- src/infrastructure/team_generator.py
+++ src/infrastructure/team_generator.py
@@ -3,6 +3,10 @@ import decimal
 
 
 
-def generate_team(players:list):
+def generate_teams(players:list):
     for player in players:
-        player['media'] = Decimal(player['Tier'])
+        player['media'] = decimal(player['Tier']) + decimal(player['Nota']) / 2
+
+    finished = Fale
+    while not finished:
+        
